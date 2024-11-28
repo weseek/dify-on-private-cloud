e6849132db96bd0793ae2ccbc0fab730f528669a
Yuki Takei <yuki@weseek.co.jp>
2024-11-28 10:25:31 +0000

    add dify-google-cloud-terraform

---
+++ dify-google-cloud-terraform/terraform/environments/dev/variables.tf
@@ -0,0 +1,72 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "dify_version" {
+  type = string
+}
+
+variable "dify_sandbox_version" {
+  type = string
+}
+
+variable "cloud_run_ingress" {
+  type = string
+}
+
+variable "nginx_repository_id" {
+  type = string
+}
+
+variable "web_repository_id" {
+  type = string
+}
+
+variable "api_repository_id" {
+  type = string
+}
+
+variable "sandbox_repository_id" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type = string
+}
+
+variable "db_port" {
+  type = string
+}
+
+variable "db_database" {
+  type = string
+}
+
+variable "storage_type" {
+  type = string
+}
+
+variable "google_storage_bucket_name" {
+  type = string
+}
+
+variable "vector_store" {
+  type = string
+}
+
+variable "indexing_max_segmentation_tokens_length" {
+  type = number
+}
+
